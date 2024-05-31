b4c3fbf83ed69bffc8e3b86b9c3cad0cf9f0bbe2
Salman Maqbool <salmanmaq@gmail.com>
2024-05-31 20:47:05 +0500

    Removed blog page

--- components/Header/menuData.tsx
+++ components/Header/menuData.tsx
@@ -14,12 +14,6 @@ const menuData: Menu[] = [
     newTab: false,
   },
   {
-    id: 33,
-    title: "Blog",
-    path: "/blog",
-    newTab: false,
-  },
-  {
     id: 3,
     title: "Support",
     path: "/contact",
@@ -40,12 +34,6 @@ const menuData: Menu[] = [
         id: 42,
         title: "Contact Page",
         path: "/contact",
-        newTab: false,
-      },
-      {
-        id: 43,
-        title: "Blog Grid Page",
-        path: "/blog",
         newTab: false,
       },
       {
