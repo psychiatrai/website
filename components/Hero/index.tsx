9eba8035ed74a5398afd408ca62c7676adb3469c
aqib-baloch <hafizaqibbaloch@gmail.com>
2025-08-07 10:45:27 +0500

    upgrade the library versions and fix the issues

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -27,7 +27,7 @@ const Hero = () => {
                 </p>
                 <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
                   <Link
-                    href="#waitlist"
+                    href="/#waitlist"
                     className="rounded-md bg-primary py-4 px-8 text-base font-semibold text-white duration-300 ease-in-out hover:bg-primary/80"
                   >
                     Sign Up Now
